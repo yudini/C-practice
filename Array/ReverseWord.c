e878150f6cf52ecafd16fe4b9b4cdc35ec781f9b
yudini <oyj0922@naver.com>
2020-11-09 12:20:57 +0900

    array

---
+++ Array/ReverseWord.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char voca[100];
+    int i;
+    int len =0;
+    char temp;
+    printf("영단어 입력: ");
+    scanf("%s",voca);
+
+    while(voca[len] != 0){
+        len++;
+    }
+    for(i=0;i<len/2;i++){
+        temp=voca[i];
+        voca[i]=voca[(len-i)-1];
+        voca[(len-i)-1]=temp;
+    }
+    printf("뒤집힌 영단어: %s \n",voca);
+    return 0;
+}
